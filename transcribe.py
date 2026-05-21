11d5aa0c571b736cd92074e0ff83ff44a83f89e0
Abhinav Diwakar <abhinav.diwakar2010@gmail.com>
2026-05-21 19:01:38 -0400

    base transcription logic, timestamping needed for .md files

---
+++ transcribe.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+from docling.datamodel import asr_model_specs
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import AsrPipelineOptions
+from docling.document_converter import AudioFormatOption, DocumentConverter
+from docling.pipeline.asr_pipeline import AsrPipeline
+
+import json
+
+OUTPUT = Path("C:\\Users\\Abhinav Diwakar\\Documents\\GitHub\\video-ingestion\\data")
+
+class TranscriptResult:
+    def __init__(self, document, markdown):
+        self.document = document
+        self.markdown = markdown
+
+def transcribe_audio (
+        audio_path: Path
+) -> TranscriptResult:    
+    audio_path = audio_path.resolve()
+    if not audio_path.exists():
+        raise FileNotFoundError
+
+    pipeline_options = AsrPipelineOptions()
+    pipeline_options.asr_options = asr_model_specs.WHISPER_TURBO
+
+    converter = DocumentConverter(
+        format_options={
+            InputFormat.AUDIO: AudioFormatOption(
+                pipeline_cls=AsrPipeline,
+                pipeline_options=pipeline_options,
+            )
+        }
+    )
+
+    result = converter.convert(audio_path)
+    doc = result.document
+
+    # Save markdown transcript
+    md = doc.export_to_markdown()
+    
+    with open("transcript.json", "w") as f:
+        json.dump(doc.export_to_dict(), f, indent=4)
+
+    return TranscriptResult(
+        document=doc,
+        markdown=md
+    )
+
+def main():
+    result = transcribe_audio(Path("E:\\Videos\\WRO\\001 - WRO #9 21-Day EMA Part 1, Webby Rambles On (Slight Return) [GxQpyUfZv4U].mp4"))
+    
+
+# def main():
+#     audio_path = Path("videoplayback.mp3")
+#     assert audio_path.exists(), f"Audio file not found: {audio_path}"
+
+#     pipeline_options = AsrPipelineOptions()
+#     pipeline_options.asr_options = asr_model_specs.WHISPER_TURBO
+
+#     converter = DocumentConverter(
+#         format_options={
+#             InputFormat.AUDIO: AudioFormatOption(
+#                 pipeline_cls=AsrPipeline,
+#                 pipeline_options=pipeline_options,
+#             )
+#         }
+#     )
+
+#     result = converter.convert(audio_path)
+#     doc = result.document
+
+#     # Save markdown transcript
+#     md = doc.export_to_markdown()
+#     Path("transcript.md").write_text(md)
+#     print("Saved transcript.md")
+#     print()
+#     print(md)
+
+if __name__ == "__main__":
+    main()
